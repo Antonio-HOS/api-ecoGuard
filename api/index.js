ede5a3e0dd7119a3d088558bbf2546fa6f27bb4c
Antonio-HOS <ahosantos.cic@uesc.br>
2024-12-08 12:53:33 -0300

    config vercel.json

--- api/index.js
+++ api/index.js
@@ -7,18 +7,21 @@ const aresRoutes = require("../routes/ares");
 const denunciaRoutes = require("../routes/denuncia");
 const sequelize = require("../config/database"); // Ajuste o caminho
 
-// Sincronização do banco de dados
+//// Sincronização do banco de dados
 sequelize
-  .sync()
-  .then(() => console.log("Database synchronized"))
-  .catch((err) => console.error("Unable to synchronize the database:", err));
+.sync()
+.then(() => console.log("Database connection established"))
+.catch((err) => console.error("Unable to connect to the database:", err));
 
 // Middlewares e rotas
 app.use(express.json());
+app.get("/", (req, res) => res.send("Express on Vercel"));
 app.use('/aguas', aguasRoutes);
 app.use('/solos', solosRoutes);
 app.use('/ares', aresRoutes);
 app.use('/denuncia', denunciaRoutes);
 
-// Exporta a aplicação como serverless
-module.exports = serverless(app);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
